b31a592225183dbbf5d9e2366bf53f61a125fabf
lzfengluo <zhichao.zhang@speedatagroup>
2019-08-29 18:26:51 +0800

    zzc:完善图片打印功能，调整画布打印预设

--- demo/src/main/java/com/spd/print/jx/pictureprint/PrintPictureActivity.java
+++ demo/src/main/java/com/spd/print/jx/pictureprint/PrintPictureActivity.java
@@ -2,12 +2,18 @@ package com.spd.print.jx.pictureprint;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.spd.lib.mvp.BaseMvpActivity;
 import com.spd.print.jx.R;
+import com.spd.print.jx.application.BaseApp;
 import com.spd.print.jx.pictureprint.contract.PrintPictureContract;
 import com.spd.print.jx.pictureprint.presenter.PrintPicturePresenter;
+import com.spd.print.jx.utils.ToastUtil;
 
 /**
  * 图片打印
@@ -15,9 +21,29 @@ import com.spd.print.jx.pictureprint.presenter.PrintPicturePresenter;
  * @author zzc
  */
 public class PrintPictureActivity extends BaseMvpActivity<PrintPicturePresenter> implements View.OnClickListener, PrintPictureContract.View {
+
+    private ImageView mIvPreview;
+
     @Override
     public void onClick(View v) {
-
+        if (BaseApp.isConnection) {
+            switch (v.getId()) {
+                case R.id.btn_print_bitmap:
+                    mPresenter.printPresetBitmap();
+                    break;
+                case R.id.btn_print_canvas:
+                    mPresenter.printCanvas();
+                    break;
+                case R.id.btn_picture_photo:
+                    mPresenter.printLocalPhoto();
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            ToastUtil.customToastView(mContext, getString(R.string.toast_error_tips), Toast.LENGTH_SHORT
+                    , (TextView) LayoutInflater.from(mContext).inflate(R.layout.layout_toast, null));
+        }
     }
 
     @Override
@@ -38,6 +64,9 @@ public class PrintPictureActivity extends BaseMvpActivity<PrintPicturePresenter>
 
     @Override
     protected void initView(@Nullable Bundle savedInstanceState) {
-
+        findViewById(R.id.btn_print_bitmap).setOnClickListener(this);
+        findViewById(R.id.btn_print_canvas).setOnClickListener(this);
+        findViewById(R.id.btn_picture_photo).setOnClickListener(this);
+        mIvPreview = findViewById(R.id.iv_picture_preview);
     }
 }
--- demo/src/main/java/com/spd/print/jx/pictureprint/presenter/PrintPicturePresenter.java
+++ demo/src/main/java/com/spd/print/jx/pictureprint/presenter/PrintPicturePresenter.java
@@ -1,9 +1,18 @@
 package com.spd.print.jx.pictureprint.presenter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+
+import com.printer.sdk.PrinterConstants;
 import com.spd.lib.mvp.BasePresenter;
+import com.spd.print.jx.R;
+import com.spd.print.jx.application.BaseApp;
 import com.spd.print.jx.pictureprint.PrintPictureActivity;
 import com.spd.print.jx.pictureprint.contract.PrintPictureContract;
 import com.spd.print.jx.pictureprint.model.PrintPictureModel;
+import com.spd.print.jx.utils.CanvasUtils;
+import com.spd.print.jx.utils.PictureUtils;
 
 /**
  * @author zzc
@@ -12,5 +21,32 @@ public class PrintPicturePresenter extends BasePresenter<PrintPictureActivity, P
     @Override
     protected PrintPictureModel createModel() {
         return new PrintPictureModel();
+    }
+
+    public void printPresetBitmap() {
+        BaseApp.getPrinterImpl().initPrinter();
+        BaseApp.getPrinterImpl().printText("\n");
+        BaseApp.getPrinterImpl().printText(getView().getString(R.string.print_bitmap_test));
+        BaseApp.getPrinterImpl().setPrinter(PrinterConstants.Command.PRINT_AND_WAKE_PAPER_BY_LINE, 1);
+        BitmapFactory.Options bfoOptions = new BitmapFactory.Options();
+        bfoOptions.inScaled = false;
+        Bitmap bitmap = BitmapFactory.decodeResource(getView().getResources(), R.drawable.my_monochrome_image, bfoOptions);
+        Matrix matrix = new Matrix();
+        //matrix.setScale(X轴缩放,Y轴缩放，，);后面两个参数是相对于缩放的位置放置，尝试设置，建议数值>100以上进行设置
+        matrix.setScale(1f, 1f);
+        Bitmap resizeBmp = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        PictureUtils.printBitmapImage(BaseApp.getPrinterImpl().getPrinter(), resizeBmp, PrinterConstants.PAlign.NONE, 0, false);
+        BaseApp.getPrinterImpl().initPrinter();
+        BaseApp.getPrinterImpl().printText("\n");
+        BaseApp.getPrinterImpl().printText(getView().getString(R.string.print_bitmap_end_test));
+        BaseApp.getPrinterImpl().printText("\n");
+    }
+
+    public void printCanvas() {
+        new CanvasUtils().printCustomImage(getView().getResources(), BaseApp.getPrinterImpl().getPrinter());
+    }
+
+    public void printLocalPhoto() {
+
     }
 }
