0d060e6150fdd797d23f2f896cd84e2afc543b1e
lzfengluo <zhichao.zhang@speedatagroup>
2019-11-19 15:15:35 +0800

    删除废弃代码

--- app/src/main/java/com/printer/demo/ymode/TimeoutException.java
+++
@@ -1,9 +0,0 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
-package com.printer.demo.ymode;
-
-class TimeoutException extends Exception {
-}
